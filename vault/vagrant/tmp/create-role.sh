a2253d0b5e9df77049ac31ee973c76b4954a5cd3
radix10 <admin@radix10.me>
2020-11-06 18:28:47 +0800

    add vault cert script

--- vault/vagrant/tmp/create-role.sh
+++ vault/vagrant/tmp/create-role.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -X POST --header "X-Vault-Token:$(cat init-result.json| jq -r .root_token)" --data @create-role-request.json https://Vault:8200/v1/pki/roles/my-role
+curl -X POST --header "X-Vault-Token:$(cat init-result.json| jq -r .root_token)" --data @create-role-request.json https://Vault:8200/v1/pki/roles/ipool.me
