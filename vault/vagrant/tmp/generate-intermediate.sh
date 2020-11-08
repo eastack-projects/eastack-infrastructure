e80c03974e4b2bf6408cdfda016e0d82cc431ea4
radix10 <admin@radix10.me>
2020-11-08 23:21:48 +0800

    add vault init

--- vault/vagrant/tmp/generate-intermediate.sh
+++ vault/vagrant/tmp/generate-intermediate.sh
@@ -3,4 +3,4 @@
 curl -X POST \
 	--header "X-Vault-Token:$(cat init-result.json| jq -r .root_token)" \
 	--data @mount-pki-request.json \
-	https://Vault:8200/v1/sys/mounts/pki
+	https://Vault:8200/v1/pki/intermediate/generate/internal | jq -r .data.csr > etcd-peer-int-ca.csr
