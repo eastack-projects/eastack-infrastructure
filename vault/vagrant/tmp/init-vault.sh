0fba8a2cbb2c9efdb59e185c5df4fe92e07efa5b
radix10 <admin@radix10.me>
2020-11-10 22:54:03 +0800

    add to etcd csr request

--- vault/vagrant/tmp/init-vault.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sudo trust anchor --store ~/desktop/ipool/infrastructures/vault/ansible/roles/common/config/files/tls.crt
-curl -X PUT --data @init-request.json https://Vault:8200/v1/sys/init | jq . > init-result.json
