f8bd88d03d942f014459ef41460cf3af4d7db677
eastack <admin@eastack.me>
2021-03-12 14:24:14 +0800

    GO DIE

--- playbooks/roles/harbor/setup/files/generate-harbor-yml.py
+++ playbooks/roles/harbor/setup/files/generate-harbor-yml.py
@@ -5,12 +5,12 @@ from yaml import CLoader as Loader, CDumper as Dumper
 
 FQDN = 'harbor.ipool.me'
 
-with open('harbor.yml.tmpl', 'r') as file:
+with open('harbor.yaml.tmpl', 'r') as file:
     content = load(file, Loader=Loader)
     content['hostname'] = FQDN
     content['https']['certificate'] = f'/opt/harbor/tls/{FQDN}.crt'
     content['https']['private_key'] = f'/opt/harbor/tls/{FQDN}.key'
     content['data_volume'] = '/opt/harbor/data'
 
-with open('harbor.yml', 'w') as file:
+with open('harbor.yaml', 'w') as file:
     dump(content, file, Dumper=Dumper)
