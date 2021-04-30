bb702848304012fb307b00b61e54fd61435022d6
eastack <admin@eastack.me>
2021-04-30 18:08:49 +0800

    tmp

--- autorun.sh
+++ autorun.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -euxo pipefail
+
 #cd vagrantfiles/vault
 #vagrant destroy -f && vagrant up
 #cd ../../playbooks
@@ -11,14 +14,14 @@
 #ansible-playbook bind.yaml
 #
 #cd ../
-
+#
 #cd vagrantfiles/etcd
 #vagrant destroy -f && vagrant up
 #cd ../../playbooks
 #ansible-playbook etcd.yaml
 #
 #cd ../
-
+#
 #cd vagrantfiles/kubernetes/server
 #vagrant destroy -f && vagrant up
 #cd ../../../playbooks
@@ -27,6 +30,6 @@
 #cd ../
 
 cd vagrantfiles/kubernetes/node
-vagrant destroy -f && vagrant up
+#vagrant destroy -f && vagrant up
 cd ../../../playbooks
 ansible-playbook k8s-node.yaml
