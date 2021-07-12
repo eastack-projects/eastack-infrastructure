408bb768b1d91a8f92d1f0a009e564c68fa3aebe
eastack <admin@eastack.me>
2021-07-12 22:15:08 +0800

    feat: back to ansible_host

--- scripts/inventory-patch.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-public_ip=$(ip -o -4 addr list enp0s8 | awk '{print $4}' | cut -d/ -f1)
-sed -i "s/127.0.0.1/$public_ip/" /vagrant/.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory
