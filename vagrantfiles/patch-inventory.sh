3dd3c0d73c5770cbce522baa6c86c06cc98d8f30
eastack <admin@eastack.me>
2021-07-12 20:50:20 +0800

    tmp

---
+++ vagrantfiles/patch-inventory.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+public_ip=$(ip -o -4 addr list enp2s0 | awk '{print $4}' | cut -d/ -f1)
+sed -i "s/127.0.0.1/$public_ip/" /vagrant/.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory
